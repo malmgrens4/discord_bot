60e8ce873ca963b8019bf84e343d10c2fbd72cd1
Steven <4smalmgren@gmail.com>
2019-08-17 19:45:40 -0400

    Added database api. DB will use peewee for an ORM.

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@ import discord
 import configparser
 import logging
 import logging.config
+import db_api
 
 logging.config.fileConfig('config.ini')
 log = logging.getLogger(__name__)
@@ -10,16 +11,26 @@ log = logging.getLogger(__name__)
 config = configparser.ConfigParser()
 config.read('config.ini')
 
-
-
 TOKEN = config['DEFAULT']['discord_bot_token']
 
 class MyClient(discord.Client):
     async def on_ready(self):
         log.info('Logged on as {0}!'.format(self.user))
 
+        for guild in self.guilds:
+            for member in guild.members:
+                db_api.init_user_stats(user={'id': member.id},
+                                       guild={'id': guild.id})
+                stats = db_api.get_user_stats(
+                        user={'id': member.id},
+                        guild={'id': guild.id})
+
+
+
     async def on_message(self, message):
         log.info('Message from {0.author}: {0.content}'.format(message))
+
+
 
 client = MyClient()
 client.run(TOKEN)
--- db_api.py
+++ db_api.py
@@ -1,22 +1,52 @@
-import sqlite3
 import configparser
 import logging
+from peewee import *
+
 
 config = configparser.ConfigParser()
 config.read('config.ini')
 
+db = SqliteDatabase(config['DEFAULT']['database_path'])
 log = logging.getLogger(__name__)
 
-def create_connection(db_file):
-    """ create a database connection to a SQLite database """
+
+# So we don't have to redefine the db every time
+class BaseModel(Model):
+    class Meta:
+        database = db
+
+class UserStats(BaseModel):
+    guild = IntegerField(null=False)
+    user = IntegerField(null=False)
+    gold = IntegerField(default=0, null=False)
+
+    class Meta:
+        primary_key = CompositeKey('guild', 'user')
+
+db.connect()
+db.create_tables([UserStats])
+
+def init_user_stats(user, guild):
     try:
-        conn = sqlite3.connect(db_file)
-        log.info(sqlite3.version)
-    except sqlite3.Error as e:
+        UserStats.create(user=user['id'], guild=guild['id'])
+    except Exception as e:
         log.error(e)
-    finally:
-        conn.close()
 
-create_connection(config['db'])
+def add_user_gold(user, guild, amount):
+    try:
+        UserStats.update(gold = UserStats.gold+amount) \
+            .where(UserStats.user == user['id'],
+                   UserStats.guild == guild['id'])
+    except Exception as e:
+        log.error(e)
 
-#def add_currency(user, amount):
+def get_user_stats(user, guild):
+    try:
+        log.info('Get user stats: User: %s Guild: %s'%(user['id'], guild['id']))
+        return UserStats.get(UserStats.user == user['id'], UserStats.guild == guild['id'])
+    except Exception as e:
+        log.error(e)
+
+
+
+
