3cabdb2284b1dc963c6e7e6442639fcece929eb7
Steven <4smalmgren@gmail.com>
2019-08-18 21:53:32 -0400

    Added websockets to get player activities and switched to @client.event decorator implementations of events

--- bot.py
+++ bot.py
@@ -4,6 +4,9 @@ import configparser
 import logging
 import logging.config
 import db_api
+import asyncio
+import aiohttp
+import json
 
 logging.config.fileConfig('config.ini')
 log = logging.getLogger(__name__)
@@ -11,26 +14,91 @@ log = logging.getLogger(__name__)
 config = configparser.ConfigParser()
 config.read('config.ini')
 
+cmd_start = config['DEFAULT']['cmd_start']
 TOKEN = config['DEFAULT']['discord_bot_token']
+URL = config['DEFAULT']['api_url']
 
-class MyClient(discord.Client):
-    async def on_ready(self):
-        log.info('Logged on as {0}!'.format(self.user))
+client = discord.Client()
 
-        for guild in self.guilds:
-            for member in guild.members:
-                db_api.init_user_stats(user={'id': member.id},
-                                       guild={'id': guild.id})
-                stats = db_api.get_user_stats(
-                        user={'id': member.id},
-                        guild={'id': guild.id})
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    print(message)
 
 
 
-    async def on_message(self, message):
-        log.info('Message from {0.author}: {0.content}'.format(message))
+async def api_call(path):
+    """Return the JSON body of a call to Discord REST API."""
+    async with aiohttp.ClientSession() as session:
+        async with session.get(f"{URL}{path}") as response:
+            assert 200 == response.status, response.reason
+            return await response.json()
+
+
+async def websocket_start():
+    """websocket start program."""
+    response = await api_call("/gateway")
+    await start(response["url"])
+
+
+async def start(url):
+    async with aiohttp.ClientSession() as session:
+        async with session.ws_connect(
+                f"{url}?v=6&encoding=json") as ws:
+            last_sequence = None
+            async for msg in ws:
+
+                data = json.loads(msg.data)
+
+                if data["op"] == 10:  # Hello
+
+                    asyncio.ensure_future(heartbeat(
+                        ws,
+                        data['d']['heartbeat_interval'],
+                        last_sequence))
+
+                    await ws.send_json({
+                        "op": 2,  # Identify
+                        "d": {
+                            "token": TOKEN,
+                            "properties": {},
+                            "compress": False,
+                            "large_threshold": 250
+                        }
+                    })
+
+                elif data["op"] == 11:
+                    # TODO
+                    # if this is not received between heartbeats
+                    # the connection is zombified
+                    pass
+                elif data["op"] == 9:
+                    log.debug("The gateway connection threshold was exceeded")
+                    pass
+                elif data["op"] == 0:  # Dispatch
+                    last_sequence = data['d']
+                    print(data['t'], data['d'])
+
+
+                else:
+                    print(data)
+
+
+async def heartbeat(ws, interval, last_sequence):
+    """Send every interval ms the heatbeat message."""
+    while True:
+        await asyncio.sleep(interval / 1000)  # seconds
+        await ws.send_json({
+            "op": 1,  # Heartbeat
+            "d": last_sequence
+        })
+
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(asyncio.gather(*[websocket_start(), client.start(TOKEN)]))
+loop.close()
 
 
 
-client = MyClient()
-client.run(TOKEN)
+
